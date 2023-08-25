acb3aea6c0791c55d7744ec1c8115a59952a6f54
Paul Ajuizeogu <paul.a@mblhightech.com>
2023-08-25 10:01:13 +0300

    Complete timeline integration

--- src/components/PlayerTimeLine.tsx
+++ src/components/PlayerTimeLine.tsx
@@ -1,26 +1,52 @@
-import { Timeline } from "antd";
+import { Tag, Timeline } from "antd";
 import { IQuestionAnswer } from "../utils/types";
+import { CheckCircleOutlined, CloseCircleOutlined } from "@ant-design/icons";
 
 interface IProps {
   result: IQuestionAnswer[];
 }
 
+const AnswerConfig = ({ rec }: { rec: IQuestionAnswer }) => {
+  if (rec.answer === rec.value) {
+    return (
+      <Tag color="green" icon={<CheckCircleOutlined />}>
+        <strong>A</strong>: {rec.value}
+      </Tag>
+    );
+  }
+
+  if (rec.answer === undefined) {
+    return <Tag color="warning">Timeout!. Answer should be {rec.answer}</Tag>;
+  }
+
+  return (
+    <>
+      <Tag color="error" icon={<CloseCircleOutlined />}>
+        <strong>A</strong>: {rec.value} <br /> Wrong!. Answer should be{" "}
+        {rec.answer}
+      </Tag>
+    </>
+  );
+};
+
 const PlayerTimeLine = ({ result }: IProps) => {
   return (
     <Timeline
       mode="alternate"
-      items={result.map((rec) => ({
-        children: (
-          <div>
-            <p>
-              <strong>Q</strong>: {rec.question}
-            </p>
-            <p>
-              <strong>A</strong>: {rec.answer}
-            </p>
-          </div>
-        ),
-      }))}
+      items={result.map((rec) => {
+        return {
+          children: (
+            <div>
+              <p>
+                <strong>Q</strong>: {rec.question}
+              </p>
+              <p>
+                <AnswerConfig rec={rec} />
+              </p>
+            </div>
+          ),
+        };
+      })}
     />
   );
 };
